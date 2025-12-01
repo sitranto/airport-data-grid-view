38aa1dc2fdb0ec6c6d8562e842dd9abe35a53349
Tonchikxx <anatoliyl2006@gmail.com>
2025-12-01 22:06:02 +0300

    Unit тесты

---
+++ BL/AirportDataGridView.Services.Tests/PlaneServiceTest.cs
@@ -0,0 +1,158 @@
+﻿using AirportDataGridView.Entities.Models;
+using AirportDataGridView.Repository.Contracts;
+using AirportDataGridView.Services.Contracts;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace AirportDataGridView.Services.Tests
+{
+    /// <summary>
+    /// Набор тестов для проверки <see cref="PlaneService"/>
+    /// </summary>
+    public class PlaneServiceTest
+    {
+        private readonly Mock<IStorage> mockStorage;
+        private readonly IService service;
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        /// <summary>
+        /// Конструктор для класса <see cref="PlaneServiceTest"/>
+        /// </summary>
+        public PlaneServiceTest()
+        {
+            mockStorage = new Mock<IStorage>();
+            service = new PlaneService(mockStorage.Object);
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        /// Проверка вызова метода добавления при добавлении в хранилище
+        /// </summary>
+        [Fact]
+        public async Task AddShouldCallStorageAdd()
+        {
+            var plane = new Plane
+            {
+                FlightNum = 1,
+                PlaneType = PlaneType.Boing,
+                Arrive = DateTime.Now.AddDays(2),
+                PassengersAmount = 10,
+                PassengersFee = 5,
+                CrewAmount = 3,
+                CrewFee = 10,
+                Markup = 15
+            };
+
+            await service.Add(plane, cancellationTokenSource.Token);
+
+            mockStorage.Verify(x => x.Add(plane, cancellationTokenSource.Token), Times.Once);
+        }
+
+        /// <summary>
+        /// Проверка вызова метода удаления при удалении из хранилища
+        /// </summary>
+        [Fact]
+        public async Task DeleteShouldCallStorageDelete()
+        {
+            var plane = new Plane();
+            await service.Add(plane);
+
+            await service.Delete(plane, cancellationTokenSource.Token);
+
+            mockStorage.Verify(x => x.Delete(plane, cancellationTokenSource.Token), Times.Once);
+        }
+
+        /// <summary>
+        /// Проверка корректности возвращаемых данных при вызове получения всех полетов
+        /// </summary>
+        [Fact]
+        public async Task GetAllShouldReturnDataFromStorage()
+        {
+            var list = new List<Plane>
+            {
+                new (),
+                new (),
+            };
+            mockStorage
+                .Setup(x => x.GetAll(cancellationTokenSource.Token))
+                .ReturnsAsync(list);
+
+            var res = await service.GetAll(cancellationTokenSource.Token);
+
+            res.Should().BeSameAs(list);
+        }
+
+        /// <summary>
+        /// Проверка корректности данных возвращаемой статистики
+        /// </summary>
+        [Fact]
+        public async Task StatisticsShouldReturnCorrectData()
+        {
+            var expectedStatistics = new PlaneStatistics
+            {
+                AllFlights = 5,
+                AllPassengers = 250,
+                AllCrew = 25,
+                AllRevenue = 50000
+            };
+
+            mockStorage.Setup(s => s.Statistics(cancellationTokenSource.Token))
+                       .ReturnsAsync(expectedStatistics);
+
+            var result = await service.Statistics(cancellationTokenSource.Token);
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(expectedStatistics);
+            mockStorage.Verify(s => s.Statistics(cancellationTokenSource.Token), Times.Once);
+        }
+
+        /// <summary>
+        /// Проверка обновления полета при вызове метода обновления
+        /// </summary>
+        [Fact]
+        public async Task UpdateShouldUpdateEntityDataInStorage()
+        {
+            var id = Guid.NewGuid();
+            var incomingPlane = new Plane
+            {
+                Id = id,
+                FlightNum = 1,
+                PlaneType = PlaneType.Boing,
+                Arrive = DateTime.Now.AddDays(2),
+                PassengersAmount = 10,
+                PassengersFee = 5,
+                CrewAmount = 3,
+                CrewFee = 10,
+                Markup = 15
+            };
+
+            mockStorage.Setup(x => x.Update(
+                It.Is<Plane>(p =>
+                    p.FlightNum == 1 &&
+                    p.PlaneType == PlaneType.Boing &&
+                    p.PassengersAmount == 10 &&
+                    p.PassengersFee == 5 &&
+                    p.CrewAmount == 3 &&
+                    p.CrewFee == 10 &&
+                    p.Markup == 15
+                ), cancellationTokenSource.Token)
+            ).Returns(Task.CompletedTask);
+
+            await service.Update(incomingPlane, cancellationTokenSource.Token);
+
+            mockStorage.Verify(s => s.Update(
+                It.Is<Plane>(p =>
+                    p.FlightNum == incomingPlane.FlightNum &&
+                    p.PlaneType == incomingPlane.PlaneType &&
+                    p.Arrive == incomingPlane.Arrive &&
+                    p.PassengersAmount == incomingPlane.PassengersAmount &&
+                    p.PassengersFee == incomingPlane.PassengersFee &&
+                    p.CrewAmount == incomingPlane.CrewAmount &&
+                    p.CrewFee == incomingPlane.CrewFee &&
+                    p.Markup == incomingPlane.Markup
+                ), cancellationTokenSource.Token), Times.Once
+            );
+        }
+    }
+}
--- BL/AirportDataGridView.Services/PlaneService.cs
+++ BL/AirportDataGridView.Services/PlaneService.cs
@@ -4,32 +4,50 @@ using AirportDataGridView.Services.Contracts;
 
 namespace AirportDataGridView.Services
 {
+    /// <summary>
+    /// Сервисный слой для работы с данными о полетах
+    /// </summary>
     public class PlaneService(IStorage storage) : IService
     {
+        /// <summary>
+        /// Добавление полета
+        /// </summary>
         public Task Add(Plane item, CancellationToken cancellationToken = default)
         {
             storage.Add(item, cancellationToken);
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Удаление полета
+        /// </summary>
         public Task Delete(Plane item, CancellationToken cancellationToken = default)
         {
             storage.Delete(item, cancellationToken);
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Возврат всех полетов
+        /// </summary>
         public async Task<IEnumerable<Plane>> GetAll(CancellationToken cancellationToken = default)
         {
             var allPLanes = await storage.GetAll(cancellationToken);
             return allPLanes;
         }
 
+        /// <summary>
+        /// Возврат статистики по полетам
+        /// </summary>
         public async Task<PlaneStatistics> Statistics(CancellationToken cancellationToken = default)
         {
             var statistics = await storage.Statistics(cancellationToken);
             return statistics;
         }
 
+        /// <summary>
+        /// Обновление данных о полете
+        /// </summary>
         public Task Update(Plane item, CancellationToken cancellationToken = default)
         {
             storage.Update(item, cancellationToken);
